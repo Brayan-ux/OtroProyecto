406530fc55827a9ac3126a909d14bfe5745705c8
Brayan GS <Bsanchez6941@gmail.com>
2024-05-25 21:05:01 -0600

    cambios carreras

--- Proyecto/index.js
+++ Proyecto/index.js
@@ -10,16 +10,15 @@ function infoEcologica() {
 
 function verDatos(tipo) {
     let info = document.getElementById("datos")
-    let fondo = document.getElementById("fondoCarreras")
 
     if(tipo == 1) {
-        info.innerText = ""
-        fondo.style.backgroundImage = "url('imgs/fondoEcoturismo.jpg')"
+        info.innerText = "los alumnos comienzan con conocimientos de los recursos naturales, se valoran y aprenden los impactos que tienen de manera turística. Los alumnos se enseñan a realizar rutas turísticas tienen prácticas profesionales respecto a hoteleria, cocina y finalizan con promoción, publicidad, mercadotecnia y  contaduría. Para esta carrera se cuenta totalmente con los espacios y utensilios necesarios para que los alumnos realicen sus prácticas profesionalmente.. "
+        info.style.backgroundImage = "url('imgs/fondoEcoturismo.jpg')"
     } else if(tipo == 2) {
-        info.innerText = ""
-        fondo.style.backgroundImage = "url('imgs/fondoProgra.jpg')"
+        info.innerText = "esta especialidad consiste en la utilización de diferentes lenguajes de programación para realizar elementos visuales,  páginas web , aplicaciones, entre otras cosas, para ello contamos con un laboratorio con los equipos necesarios,  y una biblioteca de laptops para el uso de los estudiantes."
+        info.style.backgroundImage = "url('imgs/fondoProgra.jpg')"
     } else if(tipo == 3) {
         info.innerText = ""
-        fondo.style.backgroundImage = "url('imgs/fondoMineria.jpg')"
+        info.style.backgroundImage = "url('imgs/fondoMineria.jpg')"
     }
 }
